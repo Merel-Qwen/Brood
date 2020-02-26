c9ce52931c8432c3a5ee0e31762999304a6d615e
Merel-Qwen <58332874+Merel-Qwen@users.noreply.github.com>
2020-02-26 17:42:56 +0100

    First commit

---
+++ Brood/scripts.js
@@ -0,0 +1,30 @@
+function showLess() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function Brood() {
+  let elmnt = document.getElementById("intoduction");
+  elmnt.scrollIntoView();
+}
